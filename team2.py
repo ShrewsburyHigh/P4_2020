b4037bcf2a30ffddfdbbb31652f955b0922cbc3d
KyleCheung03 <60932839+KyleCheung03@users.noreply.github.com>
2020-03-05 10:39:14 -0500

    Update team2.py

--- team2.py
+++ team2.py
@@ -1,14 +1,6 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = "East-West" # Only 10 chars displayed.
-strategy_name = "I'm nice until you cross me."
-strategy_description = "We collude except when we've been severely punished."
+team_name = "Classy Gentlemen" 
+strategy_name = "Classy"
+strategy_description = "collude for the first half. betrays for remaining rounds"
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,19 +9,11 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    if len(my_history) == 0: # It's the first round; collude.
+  
+    if len(my_history) == [1,50]: # It's the first round; collude.
         return 'c'
     else:
-        if my_history[-1]=='c' and their_history[-1]=='b':
+        if 'c' in their_history[-3:]: 
             return 'b' # Betray if severely punished last time,
         else:
             return 'c' # otherwise collude.
