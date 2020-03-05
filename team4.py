e8efe3b39f728cebab2f6d9242dc7e94349ef1d9
aagiay20 <61423605+aagiay20@users.noreply.github.com>
2020-03-05 10:18:26 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,10 +7,15 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'BlackPearl' # Only 10 chars displayed.
+strategy_name = 'Are ye ready boyo?'
+strategy_description = '''
+We start off with betray,
+then we continute with it as longs as the other team doesn't select the same one. 
+Once the other team selects the same one we switch to the other, and roll with it until they select 
+the same one again, ya feel?
+
+    '''
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -17,7 +23,14 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    
+    if 'b' in their_history[-1:]:  
+        return 'b'               
+    else:
+        if random.random()<0.1: 
+            return 'c'         
+        else:
+            return 'b'         
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,10 +38,8 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
-    
+       
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -65,4 +76,6 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b') 
+              
+            
